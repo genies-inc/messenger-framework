f4445265e5f6ae25309e560de2ea0f918d69caf8
isirin <isirin.san@gmail.com>
2017-05-23 15:23:58 +0900

    MessengerBotのgetEvents()メソッドの作成
    Eventオブジェクトの特別な場合での扱い方についての問題を説明に追加
    getEvents()メソッドのテストを作成
    テスト用の画像を追加
    curl_exec()をテスト用に上書きできるようにした

--- src/MessengerBot.php
+++ src/MessengerBot.php
@@ -29,6 +29,17 @@ class MessengerBot {
     }
   }
 
+  public function getEvents() {
+    switch ($this->type) {
+      case 'facebook':
+      return self::buildFacebookEvents($this->requestBody);
+      case 'line':
+      return self::buildLineEvents($this->requestBody);
+      default :
+      throw new \LogicException('仕様からここが実行されることはありえません。');
+    }
+  }
+
   private static function validateSignature($type, $body) {
 
     switch ($type) {
@@ -59,6 +70,122 @@ class MessengerBot {
     }
 
     return false;
+
+  }
+
+  private static function buildFacebookEvents($jsonString) {
+    $requestBody = json_decode($jsonString);
+    $events = [];
+
+    // 最下層まで展開してイベントとしての判断ができない時はからの配列を返す
+    if (!isset($requestBody->entry) || !is_array($requestBody->entry)) {
+      return [];
+    }
+
+    foreach ($requestBody->entry as $entry) {
+
+      // 最下層まで展開してイベントとしての判断ができない時はからの配列を返す
+      if (!isset($entry->messaging) || !is_array($entry->messaging)) {
+        return [];
+      }
+
+      foreach ($entry->messaging as $messaging) {
+        $event = new \stdClass();
+        $event->userId = $messaging->sender->id ?? null;
+        $event->replyToken = $messaging->sender->id ?? null;
+        $event->rawData = $requestBody;
+
+        if (isset($messaging->message)) {
+          if (isset($messaging->message->attachments)) {
+            $event->text = null;
+            $event->files = [];
+            $event->postbackData = null;
+            $typeTmp = 'Message.Image';
+            foreach ($messaging->message->attachments as $attachment) {
+              if ($attachment->type !== 'image') {
+                $typeTmp = 'invalid';
+                continue;
+              }
+              array_push($event->files, file_get_contents($attachment->payload->url));
+            }
+            $event->type = $typeTmp;
+            if ($event->type !== 'Message.Image') {
+              $event = null;
+            }
+          } elseif (isset($messaging->message->text)) {
+            $event->type = 'Message.Text';
+            $event->text = $messaging->message->text;
+            $event->files = null;
+            $event->postbackData = null;
+          } else {
+            $event = null;
+          }
+        } elseif (isset($messaging->postback)) {
+          $event->type = 'Postback';
+          $event->text = null;
+          $event->files = null;
+          $event->postbackData = $messaging->postback->payload;
+        } else {
+          $event = null;
+        }
+
+        array_push($events, $event);
+      }
+    }
+
+    return $events;
+  }
+
+  private static function buildLineEvents($jsonString) {
+    $requestBody = json_decode($jsonString);
+    $events = [];
+
+    // 最下層まで展開してイベントとしての判断ができない時はからの配列を返す
+    if (!isset($requestBody->events) || !is_array($requestBody->events)) {
+      return [];
+    }
+
+    foreach ($requestBody->events as $baseEvent) {
+      $event = new \stdClass();
+      $event->userId = $baseEvent->source->userId ?? null;
+      $event->replyToken = $baseEvent->replyToken ?? null;
+      $event->rawData = $requestBody;
+
+      if (isset($baseEvent->message)) {
+        $event->postbackData = null;
+        switch ($baseEvent->message->type) {
+          case 'text' :
+          $event->type = 'Message.Text';
+          $event->files = null;
+          $event->text = $baseEvent->message->text;
+          break;
+          case 'image' :
+          $event->type = 'Message.Image';
+          $curl = curl_init('https://api.line.me/v2/bot/message/{$baseEvent->message->id}/content');
+          $options = [
+            CURLOPT_RETURNTRANSFER => true
+          ];
+          curl_setopt_array($curl, $options);
+          $event->files = [curl_exec($curl)];
+          $event->text = null;
+          break;
+          default :
+          $event = null;
+          break;
+        }
+      } elseif (isset($baseEvent->postback)) {
+        $event->type = 'Postback';
+        $event->files = null;
+        $event->text = null;
+        $event->postbackData = $baseEvent->postback->data;
+      } else {
+        $event = null;
+      }
+
+      array_push($events, $event);
+    }
+
+    return $events;
 
   }
 
--- tests/MessengerBotTest.php
+++ tests/MessengerBotTest.php
@@ -6,8 +6,11 @@ use PHPUnit\Framework\TestCase;
 use Framework\MessengerBot;
 
 require_once './tests/utils/GLOBAL_file_get_contents-mock.php';
+require_once './tests/utils/GLOBAL_curl_exec-mock.php';
 
 class MessengerBotTest extends TestCase {
+
+  const IMAGE_PATH = './tests/resources/message_image.jpg';
 
   /**
    * @backupGlobals enabled
@@ -100,6 +103,181 @@ class MessengerBotTest extends TestCase {
     } catch (\InvalidArgumentException $e) {
       $this->addToAssertionCount(1);
     }
+  }
+
+  /**
+   * @dataProvider facebookEventsProvider
+   * @backupGlobals enabled
+   */
+  public function testGetEventsFacebook($requestBody, $expected) {
+    // Facebookからのリクエストとして設定する
+    require_once './src/config/facebook.config.php';
+    $signature = hash_hmac('sha1', $requestBody, FACEBOOK_APP_SECRET);
+    $_SERVER['HTTP_X_HUB_SIGNATURE'] = 'sha1=' . $signature;
+    global $file_get_contents_rtv;
+    $file_get_contents_rtv = $requestBody;
+    // 内部でファイル(画像)を取得する時返す画像を設定
+    global $curl_exec_rtv;
+    $curl_exec_rtv = file_get_contents(self::IMAGE_PATH);
+
+    $bot = new MessengerBot('facebook');
+    // 以降は普通のfile_get_contents()を使う
+    $file_get_contents_rtv = null;
+    $this->assertEquals($expected, $bot->getEvents());
+  }
+
+  public function facebookEventsProvider() {
+    /*
+      data case => [
+        requestBody,
+        [event -> [
+          userId, replyToken, type,
+          text, files, postbackData,
+          rawData
+        ]]
+      ]
+    */
+    return [
+      'facebook text message' => [
+        '{"object":"page","entry":[{"id":"000000000000000","time":1495206000000,"messaging":[{"sender":{"id":"1000000000000000"},"recipient":{"id":"200000000000000"},"timestamp":1495207800000,"message":{"mid":"mid.$cAADj4thus55iSabc123DEFghi45j","seq":1000,"text":"\u3066\u3059\u3068\u3066\u3059\u3068"}}]}]}',
+        [self::buildEvent(
+          '1000000000000000', '1000000000000000', 'Message.Text',
+          'てすとてすと', null, null,
+          json_decode('{"object":"page","entry":[{"id":"000000000000000","time":1495206000000,"messaging":[{"sender":{"id":"1000000000000000"},"recipient":{"id":"200000000000000"},"timestamp":1495207800000,"message":{"mid":"mid.$cAADj4thus55iSabc123DEFghi45j","seq":1000,"text":"\u3066\u3059\u3068\u3066\u3059\u3068"}}]}]}')
+        )]
+      ],
+      'facebook image message' => [
+        '{"object":"page","entry":[{"id":"000000000000000","time":1495206000000,"messaging":[{"sender":{"id":"1000000000000000"},"recipient":{"id":"200000000000000"},"timestamp":1495207800000,"message":{"mid":"mid.$cAADj4thus55iSabc123DEFghi45j","seq":1000,"attachments":[{"type":"image","payload":{"url":"./tests/resources/message_image.jpg"}}]}}]}]}',
+        [self::buildEvent(
+          '1000000000000000', '1000000000000000', 'Message.Image',
+          null, [file_get_contents(self::IMAGE_PATH)], null,
+          json_decode('{"object":"page","entry":[{"id":"000000000000000","time":1495206000000,"messaging":[{"sender":{"id":"1000000000000000"},"recipient":{"id":"200000000000000"},"timestamp":1495207800000,"message":{"mid":"mid.$cAADj4thus55iSabc123DEFghi45j","seq":1000,"attachments":[{"type":"image","payload":{"url":"./tests/resources/message_image.jpg"}}]}}]}]}')
+        )]
+      ],
+      'facebook postback' => [
+        '{"object":"page","entry":[{"id":"000000000000000","time":1495206000000,"messaging":[{"recipient":{"id":"200000000000000"},"timestamp":1495207800000,"sender":{"id":"1000000000000000"},"postback":{"payload":"text=Postback1\u0025E3\u002582\u002592\u0025E6\u00258A\u0025BC\u0025E3\u002581\u002597\u0025E3\u002581\u0025BE\u0025E3\u002581\u002597\u0025E3\u002581\u00259F"}}]}]}',
+        [self::buildEvent(
+          '1000000000000000', '1000000000000000', 'Postback',
+          null, null, 'text=Postback1%E3%82%92%E6%8A%BC%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F',
+          json_decode('{"object":"page","entry":[{"id":"000000000000000","time":1495206000000,"messaging":[{"recipient":{"id":"200000000000000"},"timestamp":1495207800000,"sender":{"id":"1000000000000000"},"postback":{"payload":"text=Postback1\u0025E3\u002582\u002592\u0025E6\u00258A\u0025BC\u0025E3\u002581\u002597\u0025E3\u002581\u0025BE\u0025E3\u002581\u002597\u0025E3\u002581\u00259F"}}]}]}')
+        )]
+      ],
+      'facebook incompatible message' => [
+        '{"object":"page","entry":[{"id":"000000000000000","time":1495206000000,"messaging":[{"sender":{"id":"1000000000000000"},"recipient":{"id":"200000000000000"},"timestamp":1495207800000,"message":{"mid":"mid.$cAADj4thus55iSabc123DEFghi45j","seq":1000,"attachments":[{"type":"video","payload":{"url":"./tests/resources/message_video.mov"}}]}}]}]}',
+        [null]
+      ],
+      'facebook incompatible messaging' => [
+        '{"entry":[{"messaging":[{"hoge":"fuga"}]}]}',
+        [null]
+      ],
+      'facebook invalid messagings' => [
+        '{"entry":[{"messaging":"hoge"}]}',
+        []
+      ],
+      'facebook incompatible entry' => [
+        '{"entry":[{"hoge":"fuga"}]}',
+        []
+      ],
+      'facebook invalid entry' => [
+        '{"entry":"hoge"}',
+        []
+      ],
+      'facebook invalid body' => [
+        '{"hoge": "fuga"}',
+        []
+      ]
+    ];
+
+  }
+  /**
+   * @dataProvider lineEventsProvider
+   * @backupGlobals enabled
+   */
+  public function testGetEventsLine($requestBody, $expected) {
+    // Lineからのリクエストとして設定をする
+    require_once './src/config/line.config.php';
+    $_SERVER['HTTP_X_LINE_SIGNATURE'] = base64_encode(hash_hmac('sha256', $requestBody, LINE_CHANNEL_SECRET, true));
+    global $file_get_contents_rtv;
+    $file_get_contents_rtv = $requestBody;
+    // 内部でファイル(画像)を取得する時返す画像を設定
+    global $curl_exec_rtv;
+    $curl_exec_rtv = file_get_contents(self::IMAGE_PATH);
+
+    $bot = new MessengerBot('line');
+    // 以降は普通のfile_get_contents()を使う
+    $file_get_contents_rtv = null;
+    $this->assertEquals($expected, $bot->getEvents());
+  }
+
+  public function lineEventsProvider() {
+    /*
+      data case => [
+        requestBody,
+        [event -> [
+          userId, replyToken, type,
+          text, files, postbackData,
+          rawData
+        ]]
+      ]
+    */
+    return [
+      'line text message' => [
+        '{"events":[{"type":"message","replyToken":"1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f","source":{"userId":"0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0","type":"user"},"timestamp":1495206000000,"message":{"type":"text","id":"2222222222222","text":"てすと"}}]}',
+        [self::buildEvent(
+          '0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0', '1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f', 'Message.Text',
+          'てすと', null, null,
+          json_decode('{"events":[{"type":"message","replyToken":"1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f","source":{"userId":"0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0","type":"user"},"timestamp":1495206000000,"message":{"type":"text","id":"2222222222222","text":"てすと"}}]}')
+        )]
+      ],
+      'line image message' => [
+        '{"events":[{"type":"message","replyToken":"1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f","source":{"userId":"0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0","type":"user"},"timestamp":1495206000000,"message":{"type":"image","id":"2222222222222"}}]}',
+        [self::buildEvent(
+          '0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0', '1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f', 'Message.Image',
+          null, [file_get_contents(self::IMAGE_PATH)], null,
+          json_decode('{"events":[{"type":"message","replyToken":"1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f","source":{"userId":"0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0","type":"user"},"timestamp":1495206000000,"message":{"type":"image","id":"2222222222222"}}]}')
+        )]
+      ],
+      'line postback' => [
+        '{"events":[{"type":"postback","replyToken":"1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f","source":{"userId":"0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0","type":"user"},"timestamp":1495206000000,"postback":{"data":"key1=value1&key2=value2&key3=value3"}}]}',
+        [self::buildEvent(
+          '0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0', '1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f', 'Postback',
+          null, null, 'key1=value1&key2=value2&key3=value3',
+          json_decode('{"events":[{"type":"postback","replyToken":"1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f","source":{"userId":"0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0","type":"user"},"timestamp":1495206000000,"postback":{"data":"key1=value1&key2=value2&key3=value3"}}]}')
+        )]
+      ],
+      'line incompatible message' => [
+        '{"events":[{"type":"message","replyToken":"1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f","source":{"userId":"0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0","type":"user"},"timestamp":1495206000000,"message":{"type":"video","id":"2222222222222"}}]}',
+        [null]
+      ],
+      'line incompatible event' => [
+        '{"events":[{"hoge":"fuga"}]}',
+        [null]
+      ],
+      'line invalid events' => [
+        '{"events":"hoge"}',
+        []
+      ],
+      'line invalid body' => [
+        '{"hoge": "fuga"}',
+        []
+      ]
+    ];
+
+  }
+
+  private static function buildEvent(
+    String $userId, String $replyToken, String $type,
+    String $text = null, Array $files = null, String $postbackData = null, \stdClass $rawData
+  ) {
+    $obj = new \stdClass();
+    $obj->userId = $userId;
+    $obj->replyToken = $replyToken;
+    $obj->type = $type;
+    $obj->text = $text;
+    $obj->files = $files;
+    $obj->postbackData = $postbackData;
+    $obj->rawData = $rawData;
+    return $obj;
   }
 
 }
