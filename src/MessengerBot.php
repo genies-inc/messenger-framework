009ae131b86fdba484ef35ac026eed6729154c3f
isirin <isirin.san@gmail.com>
2017-06-13 11:57:46 +0900

    MessengerBotのメソッドの並び整理、MARKコメントを追加、いらないプロパティの削除

--- src/MessengerBot.php
+++ src/MessengerBot.php
@@ -5,17 +5,8 @@ namespace  MessengerFramework;
 // MessengerBotクラスを使う側が何のBotかは最初に指定して使うのでリクエストから判別するような機能はいらない
 class MessengerBot {
 
-  private $requestBody = '';
-
-  private $type = '';
-
-  public $core;
-
   public function __construct($botType) {
-    $this->requestBody = file_get_contents("php://input");
-    $this->type = strtolower($botType);
-
-    switch ($this->type) {
+    switch (strtolower($botType)) {
       case 'facebook' :
       $this->core = new FacebookBot(new Curl());
       break;
@@ -25,48 +16,32 @@ class MessengerBot {
       default :
       throw new \InvalidArgumentException("指定されたプラットフォームはサポートされていません。", 1);
     }
-
   }
 
+  // MARK : Public MessengerBotのメソッド
+
+  public $core;
+
   public function getEvents() {
-    if (!$this->validateSignature()) {
+    $requestBody = file_get_contents("php://input");
+    if (!$this->validateSignature($requestBody)) {
       throw new \UnexpectedValueException("正しい送信元からのリクエストではありません。");
     }
-    switch ($this->type) {
-      case 'facebook':
-      case 'line':
-      return $this->core->parseEvents($this->requestBody);
-      default :
-      throw new \LogicException('仕様からここが実行されることはありえません。');
-    }
+    return $this->core->parseEvents($requestBody);
   }
 
   public function reply(String $replyToken) {
-    switch ($this->type) {
-      case 'facebook' :
-      case 'line' :
-      return $this->core->replyMessage($replyToken);
-      break;
-      default :
-      throw new \LogicException('仕様からここが実行されることはありえません。');
-    }
+    return $this->core->replyMessage($replyToken);
   }
 
   public function push(String $recipientId) {
-    switch ($this->type) {
-      case 'facebook' :
-      case 'line' :
-      return $this->core->pushMessage($recipientId);
-      break;
-      default :
-      throw new \LogicException('仕様からここが実行されることはありえません。');
-    }
+    return $this->core->pushMessage($recipientId);
   }
 
   public function addText(String $message) {
-    switch ($this->type) {
-      case 'facebook' :
-      case 'line' :
+    switch (true) {
+      case $this->core instanceof FacebookBot :
+      case $this->core instanceof LineBot :
       $this->core->addText($message);
       break;
       default :
@@ -74,23 +49,10 @@ class MessengerBot {
     }
   }
 
-  public function addTemplate(Array $columns) {
-    switch ($this->type) {
-      case 'facebook' :
-      $this->core->addGeneric($columns);
-      break;
-      case 'line' :
-      $this->core->addCarousel($columns);
-      break;
-      default :
-      throw new \LogicException('仕様からここが実行されることはありえません。');
-    }
-  }
-
   public function addImage(String $fileUrl, String $previewUrl) {
-    switch ($this->type) {
-      case 'facebook' :
-      case 'line' :
+    switch (true) {
+      case $this->core instanceof FacebookBot :
+      case $this->core instanceof LineBot :
       $this->core->addImage($fileUrl, $previewUrl);
       break;
       default :
@@ -99,9 +61,9 @@ class MessengerBot {
   }
 
   public function addVideo(String $fileUrl, String $previewUrl) {
-    switch ($this->type) {
-      case 'facebook' :
-      case 'line' :
+    switch (true) {
+      case $this->core instanceof FacebookBot :
+      case $this->core instanceof LineBot :
       $this->core->addVideo($fileUrl, $previewUrl);
       break;
       default :
@@ -110,9 +72,9 @@ class MessengerBot {
   }
 
   public function addAudio(String $fileUrl, Int $duration) {
-    switch ($this->type) {
-      case 'facebook' :
-      case 'line' :
+    switch (true) {
+      case $this->core instanceof FacebookBot :
+      case $this->core instanceof LineBot :
       $this->core->addAudio($fileUrl, $duration);
       break;
       default :
@@ -123,11 +85,24 @@ class MessengerBot {
   // はい、いいえにボタンを設定できる
   // つまりURLやPostbackを設定できるのだがLineではMessageにしないと発言内容が出ない
   public function addConfirm(String $text, Array $buttons) {
-    switch ($this->type) {
-      case 'facebook' :
+    switch (true) {
+      case $this->core instanceof FacebookBot :
       return $this->core->addButton($text, $buttons);
-      case 'line' :
+      case $this->core instanceof LineBot :
       return $this->core->addConfirm($text, $buttons);
+      default :
+      throw new \LogicException('仕様からここが実行されることはありえません。');
+    }
+  }
+
+  public function addTemplate(Array $columns) {
+    switch (true) {
+      case $this->core instanceof FacebookBot :
+      $this->core->addGeneric($columns);
+      break;
+      case $this->core instanceof LineBot :
+      $this->core->addCarousel($columns);
+      break;
       default :
       throw new \LogicException('仕様からここが実行されることはありえません。');
     }
@@ -136,22 +111,14 @@ class MessengerBot {
   // どのプラットフォームのイベントかはMessengerBotの状態に依存する
   // 渡されたメッセージがFacebookのものであってもnew MessengerBot('line')だったらLineとして解釈
   public function getFilesIn(Event $message) {
-    switch ($this->type) {
-      case 'facebook' :
-      return $this->core->getFiles($message);
-      break;
-      case 'line' :
-      return $this->core->getFile($message);
-      break;
-      default :
-      throw new \LogicException('仕様からここが実行されることはありえません。');
-    }
+    return $this->core->getFiles($message);
   }
 
+  // TODO : 分ける処理を下に押し込む
   public function getProfile($userId) {
     $profile = $this->core->getProfile($userId);
-    switch ($this->type) {
-      case 'facebook' :
+    switch (true) {
+      case $this->core instanceof FacebookBot :
       if (!isset($profile->first_name)) {
         throw new \UnexpectedValueException('プロフィールが取得できませんでした。');
       }
@@ -159,7 +126,7 @@ class MessengerBot {
         'name' => $profile->first_name . ' ' . $profile->last_name,
         'profilePic' => $profile->profile_pic
       ];
-      case 'line' :
+      case $this->core instanceof LineBot :
       if (!isset($profile->displayName)) {
         throw new \UnexpectedValueException('プロフィールが取得できませんでした。');
       }
@@ -172,19 +139,21 @@ class MessengerBot {
     }
   }
 
-  private function validateSignature() {
-    switch ($this->type) {
-      case 'facebook' :
+  // MARK : Private
+
+  private function validateSignature($requestBody) {
+    switch (true) {
+      case $this->core instanceof FacebookBot :
       $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? 'invalid';
       break;
-      case 'line' :
+      case $this->core instanceof LineBot :
       $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'] ?? 'invalid';
       break;
       default :
       break;
     }
 
-    return $this->core->testSignature($this->requestBody, $signature);
+    return $this->core->testSignature($requestBody, $signature);
   }
 
 }
