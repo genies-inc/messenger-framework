cff97b7b03b834f71d04249e921a01bc9ec84d54
isirin <isirin.san@gmail.com>
2017-08-12 18:21:46 +0900

    Add LineBotのボタンActionの指定をMessengerFrameworkとしてサポートするもの以外にも対応

--- src/LineBot.php
+++ src/LineBot.php
@@ -421,6 +421,13 @@ class LineBot {
       break;
       default :
     }
+    foreach ($source as $key => $value) {
+      if ($key !== 'url' && $key !== 'action' && $key !== 'title' && $key !== 'data') {
+        // 変換用のキーに属さないものはそのまま追加する
+        // つまり変換後のキーがあった場合そっちが優先される
+        $action[$key] = $value;
+      }
+    }
     return $action;
   }
 
