d44139aba434fdf62e3099fb2f805326414024e2
isirin <isirin.san@gmail.com>
2017-09-25 11:06:12 +0900

    Fix PSR2に合わせたインデントに修正

--- src/Config.php
+++ src/Config.php
@@ -18,88 +18,95 @@ namespace MessengerFramework;
  * @access public
  * @package MessengerFramework
  */
-class Config {
+class Config
+{
 
   // MARK : Constructor
 
-  /**
-   * Config constructor
-   * 二番目以降の引数を指定しなかったら環境変数から設定が読み込まれる
-   *
-   * @param String $platform
-   * @param String[] $keys シークレット、トークンの順
-   */
-  public function __construct(String $platform, ...$keys) {
-    $this->_platform = $platform;
-    switch ($platform) {
-      case 'facebook' :
-      $this->_FACEBOOK_APP_SECRET = $keys[0] ?? \getenv('FACEBOOK_APP_SECRET');
-      $this->_FACEBOOK_ACCESS_TOKEN = $keys[1] ?? \getenv('FACEBOOK_ACCESS_TOKEN');
-      break;
-      case 'line' :
-      $this->_LINE_CHANNEL_SECRET = $keys[0] ?? \getenv('LINE_CHANNEL_SECRET');
-      $this->_LINE_ACCESS_TOKEN = $keys[1] ?? \getenv('LINE_ACCESS_TOKEN');
-      break;
+    /**
+     * Config constructor
+     * 二番目以降の引数を指定しなかったら環境変数から設定が読み込まれる
+     *
+     * @param String $platform
+     * @param String[] $keys シークレット、トークンの順
+     */
+    public function __construct(String $platform, ...$keys)
+    {
+        $this->_platform = $platform;
+        switch ($platform) {
+            case 'facebook' :
+                $this->_FACEBOOK_APP_SECRET = $keys[0] ?? \getenv('FACEBOOK_APP_SECRET');
+                $this->_FACEBOOK_ACCESS_TOKEN = $keys[1] ?? \getenv('FACEBOOK_ACCESS_TOKEN');
+                break;
+            case 'line' :
+                $this->_LINE_CHANNEL_SECRET = $keys[0] ?? \getenv('LINE_CHANNEL_SECRET');
+                $this->_LINE_ACCESS_TOKEN = $keys[1] ?? \getenv('LINE_ACCESS_TOKEN');
+                break;
+        }
     }
-  }
 
-  // MARK : Public Configクラスのメソッド
+    // MARK : Public Configクラスのメソッド
 
-  /**
-   * どのプラットフォームのConfigかを取得する
-   *
-   * @return String プラットフォームの名前
-   */
-  public function getPlatform() {
-    return $this->_platform;
-  }
+    /**
+     * どのプラットフォームのConfigかを取得する
+     *
+     * @return String プラットフォームの名前
+     */
+    public function getPlatform()
+    {
+        return $this->_platform;
+    }
 
-  /**
-   * Facebookのアプリシークレットを取得する
-   *
-   * @return String Facebookのアプリシークレット
-   */
-  public function getFacebookAppSecret() {
-    return $this->_FACEBOOK_APP_SECRET;
-  }
+    /**
+     * Facebookのアプリシークレットを取得する
+     *
+     * @return String Facebookのアプリシークレット
+     */
+    public function getFacebookAppSecret()
+    {
+        return $this->_FACEBOOK_APP_SECRET;
+    }
 
-  /**
-   * Facebookのアクセストークンを取得する
-   *
-   * @return String Facebookのアクセストークン
-   */
-  public function getFacebookAccessToken() {
-    return $this->_FACEBOOK_ACCESS_TOKEN;
-  }
+    /**
+     * Facebookのアクセストークンを取得する
+     *
+     * @return String Facebookのアクセストークン
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->_FACEBOOK_ACCESS_TOKEN;
+    }
 
-  /**
-   * Lineのチャンネルシークレットを取得する
-   *
-   * @return String Lineのチャンネルシークレット
-   */
-  public function getLineChannelSecret() {
-    return $this->_LINE_CHANNEL_SECRET;
-  }
+    /**
+     * Lineのチャンネルシークレットを取得する
+     *
+     * @return String Lineのチャンネルシークレット
+     */
+    public function getLineChannelSecret()
+    {
+        return $this->_LINE_CHANNEL_SECRET;
+    }
 
-  /**
-   * Lineのアクセストークンを取得する
-   *
-   * @return String Lineのアクセストークン
-   */
-  public function getLineAccessToken() {
-    return $this->_LINE_ACCESS_TOKEN;
-  }
+    /**
+     * Lineのアクセストークンを取得する
+     *
+     * @return String Lineのアクセストークン
+     */
+    public function getLineAccessToken()
+    {
+        return $this->_LINE_ACCESS_TOKEN;
+    }
 
-  // Private
+    // Private
 
-  private $_platform;
+    private $_platform;
 
-  private $_FACEBOOK_APP_SECRET;
+    private $_FACEBOOK_APP_SECRET;
 
-  private $_FACEBOOK_ACCESS_TOKEN;
+    private $_FACEBOOK_ACCESS_TOKEN;
 
-  private $_LINE_CHANNEL_SECRET;
+    private $_LINE_CHANNEL_SECRET;
 
-  private $_LINE_ACCESS_TOKEN;
+    private $_LINE_ACCESS_TOKEN;
 
 }
